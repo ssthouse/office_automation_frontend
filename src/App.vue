ecf00cb7917b7d5a24f6f9401d7281d800be86c5
ssthouse <ssthouse@163.com>
2017-02-02 10:48:18 +0800

    Add vue third party dependencies

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img src="./assets/logo.png">
     <hello></hello>
+    <el-button @click="connectServer">Hi I'am a button from element-ui</el-button>
   </div>
 </template>
 
@@ -12,6 +13,15 @@
     name: 'app',
     components: {
       Hello
+    },
+    methods: {
+      connectServer: function () {
+        this.$http.get('http://127.0.0.1:8081/user/ssthouse').then(response => {
+          let userinfo = response.body
+          console.log(userinfo.username)
+          console.log(userinfo.age)
+        })
+      }
     }
   }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,30 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+// third party dependences
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+
+Vue.use(ElementUI)
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
+const routes = [{
+  path: '/main/:id',
+  component: App,
+  children: [
+    {path: 'item_one', component: App},
+    {path: 'item_two', component: App}]
+}]
+
+const router = new VueRouter({routes})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
   template: '<App/>',
-  components: { App }
+  components: {App}
 })
