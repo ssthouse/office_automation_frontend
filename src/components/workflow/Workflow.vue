e7a75679a58dedd47b76714e009198927da50704
ssthouse <ssthouse@163.com>
2017-02-22 21:25:23 +0800

    尝试更改本地 git 用户邮箱

--- src/components/workflow/Workflow.vue
+++ src/components/workflow/Workflow.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-    This is the workflow.
+  <div class="main_div">
+    <el-tabs type="card"
+             class="tabs">
+      <!--主标签-->
+      <el-tab-pane label="流程审批">
+        <el-col :span="12">
+        </el-col>
+      </el-tab-pane>
+
+      <!--动态增减标签-->
+      <el-tabs-pane closable="true"
+                    v-for="tab in tabs"
+                    :label="tab.title">
+
+      </el-tabs-pane>
+    </el-tabs>
   </div>
 </template>
 
@@ -10,10 +24,24 @@
     data () {
       return {}
     },
+    computed: {
+      tabs () {
+        return ''
+      }
+    },
     props: []
   }
 </script>
 
 <style>
+  .main_div {
+    width: 100%;
+    height: 100%;
+  }
 
+  .tabs {
+    background-color: white;
+    width: 100%;
+    height: 100%;
+  }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import portalModule from './modules/portal'
 
+// 配置 VUEX
 Vue.use(Vuex)
-
 const debug = process.env.NODE_ENV !== 'production'
 
 export default new Vuex.Store({
