ccd458dceac18437f25fdf1666635480713a89e6
ssthouse <ssthouse@163.com>
2017-04-17 15:12:44 +0800

    Fix fab in workflow fix in the bottom

--- src/components/workflow/Workflow.vue
+++ src/components/workflow/Workflow.vue
@@ -1,20 +1,44 @@
 <template>
-  <base-item
-    :tabs="tabs"
-    :activeTabIndex="activeTabIndex"
-    style="height: 100%;"
-    v-on:remove-tab="handleRemoveTab"
-    mainTabName="审批管理"
-    :mainTabIs="MainTab.name"></base-item>
+  <div>
+    <base-item
+      :tabs="tabs"
+      :activeTabIndex="activeTabIndex"
+      style="height: 100%;"
+      v-on:remove-tab="handleRemoveTab"
+      mainTabName="审批管理"
+      :mainTabIs="MainTab.name"></base-item>
 
+    <md-button class="md-fab md-fab-bottom-right"
+               style="margin-bottom: 60px;"
+               id="btnAdd"
+               @click.native="onClickAddWorkflow()">
+      <md-icon>add</md-icon>
+    </md-button>
+
+    <!--dialog to select new workflow type-->
+    <md-dialog md-open-from="#btnAdd" md-close-from="#btnAdd" ref="dialogAdd">
+      <md-dialog-title>新建审批</md-dialog-title>
+      <md-dialog-content>
+        <md-list>
+          <md-button @click.native="onClickAskLeave()">请假</md-button>
+          <md-button @click.native="onClickWorkOvertime()">加班</md-button>
+          <md-button @click.native="onClickOuting()">出差</md-button>
+        </md-list>
+      </md-dialog-content>
+    </md-dialog>
+  </div>
 </template>
 
 <script>
+  import TabItem from '../base/TabItem'
   import * as types from '../../store/mutation-types'
-  import {instance} from '../base/EventBus'
-  import * as Cons from '../base/Constant'
+  import * as EventBus from '../base/EventBus'
   import BaseItem from '../base/BaseItem.vue'
   import MainTab from './MainTab.vue'
+  // new workflows
+  import AskLeave from './ask_leave/AskLeave.vue'
+  import WorkOvertime from './work_overtime/WorkOvertime.vue'
+  import Outing from './outing/Outing.vue'
 
   import Vue from 'vue'
 
@@ -39,11 +63,26 @@
         let index = parseInt(tabName) - 1
         let tabIs = this.tabs[index].tabIs
         this.$store.commit(types.WORKFLOW_REMOVE_TAB, tabIs)
+      },
+      onClickAddWorkflow () {
+        this.$refs['dialogAdd'].open()
+      },
+      onClickAskLeave () {
+        this.$store.commit(types.WORKFLOW_ADD_TAB, new TabItem('新建请假流程', AskLeave.name, null))
+        this.$refs['dialogAdd'].close()
+      },
+      onClickWorkOvertime () {
+        this.$store.commit(types.WORKFLOW_ADD_TAB, new TabItem('新建加班审批', WorkOvertime.name, null))
+        this.$refs['dialogAdd'].close()
+      },
+      onClickOuting () {
+        this.$store.commit(types.WORKFLOW_ADD_TAB, new TabItem('新建出差', Outing.name, null))
+        this.$refs['dialogAdd'].close()
       }
     },
     created: function () {
       // register the tab change event
-      instance.$on(Cons.EVENT_WORKFLOW_NEW_TAB, () => {
+      EventBus.instance.$on(EventBus.EVENT_WORKFLOW_NEW_TAB, () => {
         let index = this.tabs.length
         this.activeTabIndex = index.toString()
       })
